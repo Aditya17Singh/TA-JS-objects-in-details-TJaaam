deea8ac6253a480c06b6765a233c27a252faaf46
ADITYA NARAYAN SINGH <3daditya@gmail.com>
2021-07-09 15:12:28 +0530

    refactor

--- block-BJaahs/code/index.js
+++ block-BJaahs/code/index.js
@@ -1,4 +1,4 @@
-const testData = {
+let testData = {
     title: 'Where is the capital of Jordan',
     options: ['Tashkent', 'Amaan', 'Kuwait City', 'Nairobi'],
     correctAnswerIndex: 1,
@@ -13,3 +13,20 @@ const testData = {
         return this.options[this.correctAnswerIndex]
     }
 };
+
+function createQuestion(title,options,correctAnswerIndex){
+    let question = {};
+    question.title= title;
+    question.options =options;
+    question.correctAnswerIndex= correctAnswerIndex;
+    question.isAnswerCorrect= function(index){
+        return index === this.correctAnswerIndex
+    };
+    question.getCorrectAnswer = function(){
+        return this.options[this.correctAnswerIndex]
+    }
+    return question
+}
+
+const firstQuestion = createQuestion('Where is the capital of Jordan',['Tashkent', 'Amaan', 'Kuwait City', 'Nairobi'],1)
+ 
